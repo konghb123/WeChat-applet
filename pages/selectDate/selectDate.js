3f954a253a7b3c18f534e413f5c6b05e37f5e8c1
konghb123 <konghb123work@gmail.com>
2020-10-29 11:40:25 +0800

    selectDate xg

--- pages/selectDate/selectDate.js
+++ pages/selectDate/selectDate.js
@@ -35,7 +35,7 @@ function getCurrentMonthDay(year, month) { //获取当前月份天数
   let d = new Date(year, month, 0);
   let monthDay = d.getDate()
   let days = []
-   d.getFullYear() === now.getFullYear() && d.getMonth() === now.getMonth() ? monthDay=now.getDate() : '' // 当前天数截至到当天
+  d.getFullYear() === now.getFullYear() && d.getMonth() === now.getMonth() ? monthDay = now.getDate() : '' // 当前天数截至到当天
   for (let day = 1; day <= monthDay; day++) {
     days.push(process(day))
   }
@@ -79,21 +79,41 @@ Page({
   },
   //判断展示页面  今日/本月/按日/按月
   option(currentDate) {
-    if (currentDate === "本日") {
+    if (currentDate === "本日") { //本日  
       this.setData({
         click: 1
       })
-    } else if (currentDate === "本月") {
+    } else if (currentDate === "本月") { //本月
       this.setData({
         model: false
       })
-    } else if (currentDate.split('-').length > 1) {
+    } else if (currentDate.split('-').length > 1) { //按日
       this.setData({
         click: 1
       })
-    } else {
+      let start = currentDate.split('-')[0].split('/')
+      let year = start[0]
+      let month = start[1]
+      let day = start[2]
       this.setData({
-        model: false
+        months: getCurrentYearMonth(year),
+        days: getCurrentMonthDay(year, month)
+      })
+      let startValue = [years.findIndex(item => item == year), this.data.months.findIndex(item => Number(item) == month), this.data.days.findIndex(item => Number(item) == month)]
+      this.setData({
+        startValue
+      })
+    } else { //按月
+      let arr = currentDate.split('/')
+      let year = arr[0]
+      let month = arr[1]
+      this.setData({
+        model: false,
+        months: getCurrentYearMonth(year),
+      })
+      let value = [years.findIndex(item => item == year), this.data.months.findIndex(item => Number(item) == month)]
+      this.setData({
+        value
       })
     }
   },
@@ -105,16 +125,39 @@ Page({
     this.setData({
       model: !this.data.model,
       click: !this.data.model ? 1 : 0
+    }, () => {
+      this.determine()
+
     })
   },
   //切换  选择哪个时间  按月选择时间/按日选择的开始时间/按日选择的结束时间
-  clickChange() {
-
+  clickChange(event) {
+    this.setData({
+      click: event.target.dataset.type
+    }, () => {
+      this.determine()
+    })
   },
-   //切换日期
-   selectDateChange(event) {
+  //再切换选择哪个模式 哪个时间的时候判断 月份数 天数
+  determine() {
+    let string = !this.data.model ? 'value' : (this.data.click === 1 ? 'startValue' : 'endValue')
+    let year = years[this.data[string][0]]
+    let month = this.data[string][1]
+    this.setData({
+      months: getCurrentYearMonth(year),
+    },()=>{
+      this.setData({
+        days:getCurrentMonthDay(year,this.data.months[month])
+      })
+    })
+    console.log(months, days)
+  },
+
+
+  //切换日期
+  selectDateChange(event) {
     let value = event.detail.value
-    this.monthDayChange(value)
+    this.monthDayChange(value) //日期切换后  月份数  天数 将会变化
   },
   // 日期切换后  月份数  天数 将会变化
   monthDayChange(value) {
@@ -125,11 +168,12 @@ Page({
         months: getCurrentYearMonth(this.data.years[value[0]])
       })
 
-    } else { //按天数选择
+    } else { //按日选择
+      let string = this.data.click === 1 ? 'startValue' : 'endValue'
       let year = this.data.click === 1 ? this.data.years[value[0]] : this.data.years[value[0]]
       let month = this.data.click === 1 ? Number(this.data.months[value[1]]) : Number(this.data.months[value[1]])
       this.setData({
-        value,
+        [string]: value,
         months: getCurrentYearMonth(year),
         days: getCurrentMonthDay(year, month)
       })
@@ -139,18 +183,40 @@ Page({
 
   //取消
   cancle() {
-
+    wx.navigateBack({
+      delta: 0,
+    })
   },
   //确认
   confirm() {
     let currentDate = ""
-    if (this.data.model) {
+    let {
+      years,
+      months,
+      days,
+      startValue,
+      endValue,
+      value
+    } = this.data
+    if (this.data.model) { //按日选择
 
-    } else {
-      let year = years[this.data.value[0]]
-      let month = months[this.data.value[1]]
+      //ios系统中,new Date()中的时间格式不能用 " - " 连接,应该用 " / "连接.否则会出现NaN
+      let start = years[startValue[0]] + '/' + months[startValue[1]] + '/' + days[startValue[2]]
+      let end = years[endValue[0]] + '/' + months[endValue[1]] + '/' + days[endValue[2]]
+      let startTime = new Date(start).getTime()
+      let endTime = new Date(end).getTime()
+
+      currentDate = startTime === endTime && (years[startValue[0]] == now.getFullYear() && Number(months[startValue[1]]) == now.getMonth() + 1) && Number(days[startValue[2]]) == now.getDate() ? '本日' : (startTime < endTime ? start + '-' + end : end + '-' + start)
+
+    } else { //按月选择
+      let year = years[value[0]]
+      console.log(months)
+      let month = months[value[1]]
       currentDate = year === now.getFullYear() && month === now.getMonth() + 1 ? '本月' : year + '/' + month
     }
+
+
+    //设置上一个页面  时间
     let pages = getCurrentPages()
     console.log(pages)
     if (pages.length > 1) {
@@ -166,6 +232,6 @@ Page({
     }
   },
 
- 
+
 
 })
